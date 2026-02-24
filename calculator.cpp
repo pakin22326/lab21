e0cd013f3023b77378df59e052528e559b244c04
pakin22326 <pakinounruen@gmail.com>
2026-02-24 15:35:26 +0700

    .

--- calculator.cpp
+++ calculator.cpp
@@ -1,66 +1,142 @@
 #include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ID_EDIT1 101
+#define ID_EDIT2 102
+#define ID_ADD   201
+#define ID_SUB   202
+#define ID_MUL   203
+#define ID_DIV   204
 
 /* This is where all the input to the window goes to */
-LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
-	switch(Message) {
-		
-		/* Upon destruction, tell the main thread to stop */
-		case WM_DESTROY: {
-			PostQuitMessage(0);
-			break;
-		}
-		
-		/* All other messages (a lot of them) are processed using default procedures */
-		default:
-			return DefWindowProc(hwnd, Message, wParam, lParam);
-	}
-	return 0;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+    switch(Message)
+    {
+        case WM_CREATE:
+        {
+            CreateWindow("STATIC", "Please input two numbers",
+    		WS_VISIBLE | WS_CHILD,
+    		20, 15, 210, 25,
+    		hwnd, NULL, NULL, NULL);
+
+			CreateWindow("EDIT", "",
+    		WS_VISIBLE | WS_CHILD | WS_BORDER,
+    		50, 45, 150, 25,
+    		hwnd, (HMENU)ID_EDIT1, NULL, NULL);
+
+			CreateWindow("EDIT", "",
+    		WS_VISIBLE | WS_CHILD | WS_BORDER,
+    		50, 80, 150, 25,
+    		hwnd, (HMENU)ID_EDIT2, NULL, NULL);
+
+			CreateWindow("BUTTON", "+",
+    		WS_VISIBLE | WS_CHILD,
+    		50,125, 35, 25,
+    		hwnd, (HMENU)ID_ADD, NULL, NULL);
+
+			CreateWindow("BUTTON", "-",
+   			WS_VISIBLE | WS_CHILD,
+    		90, 125, 35, 25,
+    		hwnd, (HMENU)ID_SUB, NULL, NULL);
+
+			CreateWindow("BUTTON", "*",
+    		WS_VISIBLE | WS_CHILD,
+    		130, 125, 35, 25,
+    		hwnd, (HMENU)ID_MUL, NULL, NULL);
+
+			CreateWindow("BUTTON", "/",
+    		WS_VISIBLE | WS_CHILD,
+    		170, 125, 35, 25,
+    		hwnd, (HMENU)ID_DIV, NULL, NULL);
+        }	
+        break;
+
+        case WM_COMMAND:
+        {
+            char buf1[50], buf2[50], resultStr[100];
+            double a, b, result = 0;
+
+            GetDlgItemText(hwnd, ID_EDIT1, buf1, 50);
+            GetDlgItemText(hwnd, ID_EDIT2, buf2, 50);
+
+            a = atof(buf1);
+            b = atof(buf2);
+
+            switch (LOWORD(wParam))
+            {
+                case ID_ADD: result = a + b; break;
+                case ID_SUB: result = a - b; break;
+                case ID_MUL: result = a * b; break;
+                case ID_DIV: result = (b != 0) ? a / b : 0; break;
+                default: return 0;
+            }
+
+            sprintf(resultStr, "%f", result);
+            MessageBox(hwnd, resultStr, "Result", MB_OK);
+        }
+        break;
+
+        /* Upon destruction, tell the main thread to stop */
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+            break;
+        }
+
+        /* Default message handler */
+        default:
+            return DefWindowProc(hwnd, Message, wParam, lParam);
+    }
+    return 0;
 }
 
-/* The 'main' function of Win32 GUI programs: this is where execution starts */
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	WNDCLASSEX wc; /* A properties struct of our window */
-	HWND hwnd; /* A 'HANDLE', hence the H, or a pointer to our window */
-	MSG msg; /* A temporary location for all messages */
+/* The 'main' function of Win32 GUI programs */
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG msg;
 
-	/* zero out the struct and set the stuff we want to modify */
-	memset(&wc,0,sizeof(wc));
-	wc.cbSize	 = sizeof(WNDCLASSEX);
-	wc.lpfnWndProc	 = WndProc; /* This is where we will send messages to */
-	wc.hInstance	 = hInstance;
-	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
-	
-	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-	wc.lpszClassName = "WindowClass";
-	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
-	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
+    memset(&wc, 0, sizeof(wc));
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc  = WndProc;
+    wc.hInstance    = hInstance;
+    wc.hCursor      = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = CreateSolidBrush(RGB(200, 0, 100));
+    wc.lpszClassName = "WindowClass";
+    wc.hIcon        = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIconSm      = LoadIcon(NULL, IDI_APPLICATION);
 
-	if(!RegisterClassEx(&wc)) {
-		MessageBox(NULL, "Window Registration Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
-		return 0;
-	}
+    if(!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, "Window Registration Failed!",
+                   "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, /* x */
-		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
-		NULL,NULL,hInstance,NULL);
+    hwnd = CreateWindowEx(
+        0,
+        "WindowClass",
+        "My Calculator",
+        WS_VISIBLE | WS_CAPTION | WS_SYSMENU, 
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        250, 200,
+        NULL, NULL, hInstance, NULL);
 
-	if(hwnd == NULL) {
-		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
-		return 0;
-	}
+    if(hwnd == NULL)
+    {
+        MessageBox(NULL, "Window Creation Failed!",
+                   "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
 
-	/*
-		This is the heart of our program where all input is processed and 
-		sent to WndProc. Note that GetMessage blocks code flow until it receives something, so
-		this loop will not produce unreasonably high CPU usage
-	*/
-	while(GetMessage(&msg, NULL, 0, 0) > 0) { /* If no error is received... */
-		TranslateMessage(&msg); /* Translate key codes to chars if present */
-		DispatchMessage(&msg); /* Send it to WndProc */
-	}
-	return msg.wParam;
+    while(GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return msg.wParam;
 }
